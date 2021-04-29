1a6adab1749cdd7cd4084780c7b4e5b58b0df709
lezojeda <lezojeda@gmail.com>
2021-04-28 21:37:54 -0300

    Entity classes

---
+++ ACM/Tests/ACM.BLTest/CustomerTest.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ACM.BL;
+
+namespace ACM.BLTest
+{
+    [TestClass]
+    public class CustomerTest
+    {
+        [TestMethod]
+        public void FullNameValid()
+        {
+            // Arrange
+            var customer = new Customer()
+            {
+                FirstName = "Bilbo",
+                LastName = "Baggins",
+            };
+
+            // Act
+            string expected = customer.FullName;
+
+            // Assert
+            Assert.AreEqual("Baggins, Bilbo", expected);
+        }
+
+        [TestMethod]
+        public void FullNameFirstNameEmpty()
+        {
+            // Arrange
+            var customer = new Customer()
+            {
+                LastName = "Baggins",
+            };
+
+            // Act
+            string expected = customer.FullName;
+
+            // Assert
+            Assert.AreEqual("Baggins", expected);
+        }
+
+        [TestMethod]
+        public void FullNameLastNameEmpty()
+        {
+            // Arrange
+            var customer = new Customer()
+            {
+                FirstName = "Bilbo",
+            };
+
+            // Act
+            string expected = customer.FullName;
+
+            // Assert
+            Assert.AreEqual("Bilbo", expected);
+        }
+    }
+}
